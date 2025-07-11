e0a1b76ae6b249a0dfb83d6d8b8a133cb9e28f2a
Programmering <teknisk+programmering-GDC2025-GR4@game.unf.dk>
2025-07-11 17:00:48 +0200

    movement,rotation,prefag

--- Assets/Scripts/playerscript.cs
+++ Assets/Scripts/playerscript.cs
@@ -1,21 +1,39 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class playerscript : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public int cooldownHit = 10;
     public float speed = 4f;
+    public InputAction moveAction;
+    public InputAction rotationAction;
+    
+    int actualCooldown = 0;
+    Rigidbody rb;
     void Start()
     {
         print("UwU i'm a cute start message UwU, you look cute today!");
-       
+        moveAction.Enable();
+        rotationAction.Enable();
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        actualCooldown += 1;
+        if (actualCooldown == cooldownHit)
+        {
+            print("pew");
+        } 
+            
         print("Update: you still look cute!");
-        transform.position += new Vector3(speed*Time.deltaTime, 0,0);
+        //transform.position += new Vector3(speed*Time.deltaTime, 0,0);
+
+        transform.position = transform.position + transform.forward  * (moveAction.ReadValue<float>() * speed* Time.deltaTime);
+        
+        transform.Rotate(0,rotationAction.ReadValue<float>(),0);
         
     }
 }
